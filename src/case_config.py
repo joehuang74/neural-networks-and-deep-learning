1d6aed19f832a8c960c1c7627b999ee3efe2dca3
j <joehuang74@users.noreply.github.com>
2015-04-17 15:31:37 +0800

    Add case_config.py for the new feature cases, update test_network_radar.py

---
+++ src/case_config.py
@@ -0,0 +1,31 @@
+
+FEATURE_DIR_ROOT = "D:\\";
+
+# Feature file directories
+SVMDATA_10minPrecAsLabel = "svmdata.10minPrecipAsLabel";
+SVMDATA_10minPrecAsLabel_HourlyAggr = "svmdata.10minPrecipAsLabel.HourlyAggr";
+SVMDATA_10minPrecAsLabel_TRAINSET = "svmdata.10minPrecipAsLabel.trainset";
+SVMDATA_10minPrecAsLabel_TESTSET = "svmdata.10minPrecipAsLabel.testset";
+SVMDATA_10minPrecAsLabel_HourlyAggr_TRAINSET = "svmdata.10minPrecipAsLabel.hourlyaggr.trainset";
+SVMDATA_10minPrecAsLabel_HourlyAggr_TESTSET = "svmdata.10minPrecipAsLabel.hourlyaggr.testset";
+
+# Feature file sub-directories for various cases
+# CASE1_SINGLE+".withObsTime" is where the raw files are placed and should not be removed
+CASE1_SINGLE = "svm_case1_merged_all_single";
+CASE2_1_STAT = "svm_case2.1_merged_all_stat";
+CASE2_2_1_JOINED_MID_MEAN = "svm_case2.2.1_merged_all_joined_mid_mean";
+CASE2_2_2_JOINED_MID_MEAN_ELEV = "svm_case2.2.2_merged_all_joined_mid_mean_elev";
+CASE2_2_3_JOINED_MID_MEAN_PROD = "svm_case2.2.3_merged_all_joined_mid_mean_prod";
+CASE2_3_1_JOINED_MID_MEAN_MAX5 = "svm_case2.3.1_merged_all_joined_mid_mean_max5";
+CASE2_3_2_JOINED_MID_MEAN_MAX5_ELEV = "svm_case2.3.2_merged_all_joined_mid_mean_max5_elev";
+CASE2_3_3_JOINED_MID_MEAN_MAX5_PROD = "svm_case2.3.3_merged_all_joined_mid_mean_max5_prod";
+CASE2_4_1_JOINED_MID_MEAN_MIN5 = "svm_case2.4.1_merged_all_joined_mid_mean_min5";
+CASE2_4_2_JOINED_MID_MEAN_MIN5_ELEV = "svm_case2.4.2_merged_all_joined_mid_mean_min5_elev";
+CASE2_4_3_JOINED_MID_MEAN_MIN5_PROD = "svm_case2.4.3_merged_all_joined_mid_mean_min5_prod";
+
+CASE3_1_JOINED_ELEV = "svm_case3.1_merged_all_joined_elev";
+CASE3_2_JOINED_PROD = "svm_case3.2_merged_all_joined_prod";
+
+#  All the file names should start with this prefix string
+#FEATURE_FILE_PREFIX = "svm_traindata.txt.1a";
+
--- src/test_network_radar.py
+++ src/test_network_radar.py
@@ -2,101 +2,114 @@ import light_format_feature_loader
 import network
 import glob
 import os
+from case_config import *
 
-result_log_file = '../data/tmp_result_log_file.txt'
-log_fp = open(result_log_file, 'w')
+#result_log_file = '../data/tmp_result_log_file.txt'
+#log_fp = open(result_log_file, 'w')
 
 rainfall_min_value = 0
 rainfall_max_value = 100
 n_rainfall_indexes = 100 # for use in vectorized form of rainfall label (number of neurons for output layer)
 size_hidden_layer = 30 # Number of neurons for hidden layer
 
-# training_data_dir = "/home/awips/sample_data/svmdata.oneHourPrecipAsLabel/svm_case1_merged"
-# training_data_dir = "/home/awips/sample_data/svmdata.10minPrecipAsLabel/svm_case1_merged_all"
-# training_data_dir = "/home/awips/sample_data/svmdata.10minPrecipAsLabel/svm_case1_merged"
-# training_data_dir = "/home/awips/sample_data/svmdata.10minPrecipAsLabel/svm_case1_merged_typhoon"
+case_list = []
+case_list.append(CASE1_SINGLE)
+case_list.append(CASE2_1_STAT)
+case_list.append(CASE2_2_1_JOINED_MID_MEAN)
+case_list.append(CASE2_2_2_JOINED_MID_MEAN_ELEV)
+case_list.append(CASE2_2_3_JOINED_MID_MEAN_PROD)
+case_list.append(CASE2_3_1_JOINED_MID_MEAN_MAX5)
+case_list.append(CASE2_3_2_JOINED_MID_MEAN_MAX5_ELEV)
+case_list.append(CASE2_3_3_JOINED_MID_MEAN_MAX5_PROD)
+case_list.append(CASE2_4_1_JOINED_MID_MEAN_MIN5)
+case_list.append(CASE2_4_2_JOINED_MID_MEAN_MIN5_ELEV)
+case_list.append(CASE2_4_3_JOINED_MID_MEAN_MIN5_PROD)
+case_list.append(CASE3_1_JOINED_ELEV)
+case_list.append(CASE3_2_JOINED_PROD)
 
-# training_data_dir = "/home/awips/sample_data/svmdata.oneHourPrecipAsLabel/svm_case2_merged"
-# training_data_dir = "/home/awips/sample_data/svmdata.10minPrecipAsLabel/svm_case2_merged_all"
-# training_data_dir = "/home/awips/sample_data/svmdata.10minPrecipAsLabel/svm_case2_merged"
-# training_data_dir = "/home/awips/sample_data/svmdata.10minPrecipAsLabel/svm_case2_merged_typhoon"
+for case_dir in case_list:
+    training_data_dir = "D:\\\\" + SVMDATA_10minPrecAsLabel_TRAINSET + "\\" + case_dir
+    #training_data_dir = "D:\\\\" + SVMDATA_10minPrecAsLabel_HourlyAggr_TRAINSET + "\\" + case_dir
+    
+    result_log_dir = "../log/" + SVMDATA_10minPrecAsLabel_TRAINSET 
+    result_log_file = result_log_dir + "/" + case_dir + ".log"
+    if not os.path.exists(result_log_dir):
+        os.makedirs(result_log_dir)
+    log_fp = open(result_log_file, 'w')
 
-# training_data_dir = "/home/awips/sample_data/svmdata.10minPrecipAsLabel/svm_case3_merged_all"
-training_data_dir = "/home/awips/sample_data/svmdata.10minPrecipAsLabel/svm_case3_merged_all_middle_value_only"
+    filepath_list = glob.glob(training_data_dir + "\svm_traindata.txt.1a.*")
 
-filepath_list = glob.glob(training_data_dir + "/svm_traindata.txt.3a.*")
+    filename_list = []
+    print "==== Training file list ===="
+    for filepath in filepath_list:
+        if os.path.basename(filepath).find("log") == -1:
+            filename_list.append(os.path.basename(filepath))
 
-filename_list = []
-print "==== Training file list ===="
-for filepath in filepath_list:
-    if os.path.basename(filepath).find("log") == -1:
-        filename_list.append(os.path.basename(filepath))
-
-# Manual setting of training files
-#filename_list = []
-#filename_list.append("svm_traindata.txt.1a.Z_0.5_0.5")
-#filename_list.append("svm_traindata.txt.1a.V_0.5_0.5")
-#filename_list.append("svm_traindata.txt.2b.9999.0_-9999.0")
-log_fp.write("===== Training file list =====\n")
-for filename in filename_list:
-    print filename
-    log_fp.write(filename + "\n")
-print ""
-
-"""
-Hyper-parameters
-"""
-epoches = 30
-mini_batch_size = 10
-training_rate = 2.0
-
-log_fp.write("training_data_dir: {0}\n".format(training_data_dir))
-log_fp.write("rainfall_min_value: {0}\n".format(rainfall_min_value))
-log_fp.write("rainfall_max_value: {0}\n".format(rainfall_max_value))
-log_fp.write("n_rainfall_indexes(Number of neurons for output-layer): {0}\n".format(n_rainfall_indexes))
-log_fp.write("size_hidden_layer(Number of neurons for hidden-layer): {0}\n".format(size_hidden_layer))
-log_fp.write("kernel type: Sigmoid\n")
-log_fp.write("epoches: {0}\n".format(epoches))
-log_fp.write("mini_batch_size: {0}\n".format(mini_batch_size))
-log_fp.write("training_rate: {0}\n".format(training_rate))
-
-"""
-Train and evaluate the network for the given list of training files
-"""
-for filename in filename_list:
-    training_file = training_data_dir + "/" + filename
-    training_data, validation_data, test_data, dimension = \
-    light_format_feature_loader.load_data_wrapper(training_file, rainfall_min_value, rainfall_max_value, n_rainfall_indexes)
-
-    if training_data == None:
-        print "[ERROR] Training data in {0} is None, skipped training task".format(training_file)
-        continue
-
-    log_fp.write("===== {0} =====\n".format(filename));
-    log_fp.write("dimension(Number of neurons for input layer) : {0}\n".format(dimension))
-    log_fp.write("number of training examples: {0}  test examples: {1}\n".format(len(training_data), len(test_data)))
-
-    """ 
-    Build a three-layer neural network given the number of neurons for each layer
-    Parameters:
-    [number of neurons for input layer], [number of neurons for hidden layer], [number of neurons for output layer]
-    """
-    net = network.Network([dimension, size_hidden_layer, n_rainfall_indexes])
+    # Manual setting of training files
+    #filename_list = []
+    #filename_list.append("svm_traindata.txt.1a.Z_0.5_0.5")
+    #filename_list.append("svm_traindata.txt.1a.V_0.5_0.5")
+    #filename_list.append("svm_traindata.txt.2b.9999.0_-9999.0")
+    log_fp.write("===== Training file list =====\n")
+    for filename in filename_list:
+        print filename
+        log_fp.write(filename + "\n")
+    print ""
 
     """
-    Perform stochastic gradient descent algorithm given necessary hyper-parameters
-    Parameters:
-    [training_data], [number of epoches], [mini-batch size], [training rate], [test_data]
-    net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+    Hyper-parameters
     """
-    print "===== Start training  for {0} =====".format(filename)
-    net.SGD(training_data, epoches, mini_batch_size, training_rate, log_file_pointer=log_fp, test_data=test_data)
-    correctly_classified = net.evaluate(test_data)
-    print "Result after {0} epoches: {1} / {2}  ({3}%)\n".format(
+    epoches = 3
+    mini_batch_size = 10
+    training_rate = 2.0
+
+    log_fp.write("training_data_dir: {0}\n".format(training_data_dir))
+    log_fp.write("rainfall_min_value: {0}\n".format(rainfall_min_value))
+    log_fp.write("rainfall_max_value: {0}\n".format(rainfall_max_value))
+    log_fp.write("n_rainfall_indexes(Number of neurons for output-layer): {0}\n".format(n_rainfall_indexes))
+    log_fp.write("size_hidden_layer(Number of neurons for hidden-layer): {0}\n".format(size_hidden_layer))
+    log_fp.write("kernel type: Sigmoid\n")
+    log_fp.write("epoches: {0}\n".format(epoches))
+    log_fp.write("mini_batch_size: {0}\n".format(mini_batch_size))
+    log_fp.write("training_rate: {0}\n\n".format(training_rate))
+
+    """
+    Train and evaluate the network for the given list of training files
+    """
+    for filename in filename_list:
+        training_file = training_data_dir + "/" + filename
+        training_data, validation_data, test_data, dimension = \
+        light_format_feature_loader.load_data_wrapper(training_file, rainfall_min_value, rainfall_max_value, n_rainfall_indexes)
+   
+        if training_data == None:
+            print "[ERROR] Training data in {0} is None, skipped training task".format(training_file)
+            continue
+
+        log_fp.write("===== {0} =====\n".format(filename));
+        log_fp.write("dimension(Number of neurons for input layer) : {0}\n".format(dimension))
+        log_fp.write("number of training examples: {0}  test examples: {1}\n".format(len(training_data), len(test_data)))
+
+        """ 
+        Build a three-layer neural network given the number of neurons for each layer
+        Parameters:
+        [number of neurons for input layer], [number of neurons for hidden layer], [number of neurons for output layer]
+        """
+        net = network.Network([dimension, size_hidden_layer, n_rainfall_indexes])
+
+        """
+        Perform stochastic gradient descent algorithm given necessary hyper-parameters
+        Parameters:
+        [training_data], [number of epoches], [mini-batch size], [training rate], [test_data]
+        net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+        """
+        print "===== Start training  for {0} =====".format(filename)
+        net.SGD(training_data, epoches, mini_batch_size, training_rate, log_file_pointer=log_fp, test_data=test_data)
+        correctly_classified = net.evaluate(test_data)
+        print "Result after {0} epoches: {1} / {2}  ({3}%)\n".format(
                     epoches, correctly_classified, len(test_data), 100*correctly_classified/len(test_data))
-    log_fp.write("Result after {0} epoches: {1} / {2}  ({3}%)\n".format(
+        log_fp.write("Result after {0} epoches: {1} / {2}  ({3}%)\n".format(
                     epoches, correctly_classified, len(test_data), 100*correctly_classified/len(test_data)))
-    log_fp.write("\n")
+        log_fp.write("\n")
 
-log_fp.close()
+    log_fp.close()
 
