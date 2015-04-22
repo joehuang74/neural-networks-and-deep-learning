bbadd7e51540b62e8694c0da2b897dece084d9bf
j <j@localhost>
2015-04-22 10:46:09 +0800

    Update case_config settings

--- src/case_config.py
+++ src/case_config.py
@@ -13,11 +13,12 @@ SVMDATA_10minPrecAsLabel_HourlyAggr_TRAINSET = "svmdata.10minPrecipAsLabel.Hourl
 SVMDATA_10minPrecAsLabel_HourlyAggr_TESTSET = "svmdata.10minPrecipAsLabel.HourlyAggr.testset";
 SVMDATA_10minPrecAsLabel_FuzzyLabel_TRAINSET = "svmdata.10minPrecipAsLabel.FuzzyLabel.trainset";
 SVMDATA_10minPrecAsLabel_FuzzyLabel_TESTSET = "svmdata.10minPrecipAsLabel.FuzzyLabel.testset";
-SVMDATA_10minPrecAsLabel_HourlyAggr_TRAINSET = "svmdata.10minPrecipAsLabel.HourlyAggr.FuzzyLabel.trainset";
-SVMDATA_10minPrecAsLabel_HourlyAggr_TESTSET = "svmdata.10minPrecipAsLabel.HourlyAggr.FuzzyLabel.testset";
+SVMDATA_10minPrecAsLabel_HourlyAggr_FuzzyLabel_TRAINSET = "svmdata.10minPrecipAsLabel.HourlyAggr.FuzzyLabel.trainset";
+SVMDATA_10minPrecAsLabel_HourlyAggr_FuzzyLabel_TESTSET = "svmdata.10minPrecipAsLabel.HourlyAggr.FuzzyLabel.testset";
 
 # Feature file sub-directories for various cases
-# CASE1_SINGLE+".withObsTime" is where the raw files are placed and should not be removed
+# CASE1_SINGLE+".withObsTime" is where the raw files are placed and should not be removed,
+# all the other directories can be derived by using the featureiomanager project.
 CASE1_SINGLE = "svm_case1_merged_all_single";
 CASE2_1_STAT = "svm_case2.1_merged_all_stat";
 CASE2_2_1_JOINED_MID_MEAN = "svm_case2.2.1_merged_all_joined_mid_mean";
@@ -29,9 +30,76 @@ CASE2_3_3_JOINED_MID_MEAN_MAX5_PROD = "svm_case2.3.3_merged_all_joined_mid_mean_
 CASE2_4_1_JOINED_MID_MEAN_MIN5 = "svm_case2.4.1_merged_all_joined_mid_mean_min5";
 CASE2_4_2_JOINED_MID_MEAN_MIN5_ELEV = "svm_case2.4.2_merged_all_joined_mid_mean_min5_elev";
 CASE2_4_3_JOINED_MID_MEAN_MIN5_PROD = "svm_case2.4.3_merged_all_joined_mid_mean_min5_prod";
-
+CASE2_5_1_JOINED_MID_MEAN_MAX50 = "svm_case2.5.1_merged_all_joined_mid_mean_max50";
+CASE2_5_2_JOINED_MID_MEAN_MAX50_ELEV = "svm_case2.5.2_merged_all_joined_mid_mean_max50_elev";
+CASE2_5_3_JOINED_MID_MEAN_MAX50_PROD = "svm_case2.5.3_merged_all_joined_mid_mean_max50_prod";
+CASE2_6_1_JOINED_MID_MEAN_MIN50 = "svm_case2.6.1_merged_all_joined_mid_mean_min50";
+CASE2_6_2_JOINED_MID_MEAN_MIN50_ELEV = "svm_case2.6.2_merged_all_joined_mid_mean_min50_elev";
+CASE2_6_3_JOINED_MID_MEAN_MIN50_PROD = "svm_case2.6.3_merged_all_joined_mid_mean_min50_prod";
+CASE2_7_1_JOINED_MID_MEAN_MAX50_MIN50 = "svm_case2.7.1_merged_all_joined_mid_mean_max50_min50";
+CASE2_7_2_JOINED_MID_MEAN_MAX50_MIN50_ELEV = "svm_case2.7.2_merged_all_joined_mid_mean_max50_min50_elev";
+CASE2_7_3_JOINED_MID_MEAN_MAX50_MIN50_PROD = "svm_case2.7.3_merged_all_joined_mid_mean_max50_min50_prod";
 CASE3_1_JOINED_ELEV = "svm_case3.1_merged_all_joined_elev";
 CASE3_2_JOINED_PROD = "svm_case3.2_merged_all_joined_prod";
+
+
+# List of all cases
+CASE_LIST_ALL = []
+CASE_LIST_ALL.append(CASE1_SINGLE)
+CASE_LIST_ALL.append(CASE2_1_STAT)
+CASE_LIST_ALL.append(CASE2_2_1_JOINED_MID_MEAN)
+CASE_LIST_ALL.append(CASE2_2_2_JOINED_MID_MEAN_ELEV)
+CASE_LIST_ALL.append(CASE2_2_3_JOINED_MID_MEAN_PROD)
+CASE_LIST_ALL.append(CASE2_3_1_JOINED_MID_MEAN_MAX5)
+CASE_LIST_ALL.append(CASE2_3_2_JOINED_MID_MEAN_MAX5_ELEV)
+CASE_LIST_ALL.append(CASE2_3_3_JOINED_MID_MEAN_MAX5_PROD)
+CASE_LIST_ALL.append(CASE2_4_1_JOINED_MID_MEAN_MIN5)
+CASE_LIST_ALL.append(CASE2_4_2_JOINED_MID_MEAN_MIN5_ELEV)
+CASE_LIST_ALL.append(CASE2_4_3_JOINED_MID_MEAN_MIN5_PROD)
+CASE_LIST_ALL.append(CASE2_5_1_JOINED_MID_MEAN_MAX50)
+CASE_LIST_ALL.append(CASE2_5_2_JOINED_MID_MEAN_MAX50_ELEV)
+CASE_LIST_ALL.append(CASE2_5_3_JOINED_MID_MEAN_MAX50_PROD)
+CASE_LIST_ALL.append(CASE2_6_1_JOINED_MID_MEAN_MIN50)
+CASE_LIST_ALL.append(CASE2_6_2_JOINED_MID_MEAN_MIN50_ELEV)
+CASE_LIST_ALL.append(CASE2_6_3_JOINED_MID_MEAN_MIN50_PROD)
+CASE_LIST_ALL.append(CASE2_7_1_JOINED_MID_MEAN_MAX50_MIN50)
+CASE_LIST_ALL.append(CASE2_7_2_JOINED_MID_MEAN_MAX50_MIN50_ELEV)
+CASE_LIST_ALL.append(CASE2_7_3_JOINED_MID_MEAN_MAX50_MIN50_PROD)
+CASE_LIST_ALL.append(CASE3_1_JOINED_ELEV)
+CASE_LIST_ALL.append(CASE3_2_JOINED_PROD)
+
+# List of cases in experiment 1
+CASE_LIST_EXP1 = []
+CASE_LIST_EXP1.append(CASE1_SINGLE)
+#CASE_LIST_EXP1.append(CASE2_1_STAT)
+#CASE_LIST_EXP1.append(CASE2_2_1_JOINED_MID_MEAN)
+#CASE_LIST_EXP1.append(CASE2_2_2_JOINED_MID_MEAN_ELEV)
+###CASE_LIST_EXP1.append(CASE2_2_3_JOINED_MID_MEAN_PROD)
+CASE_LIST_EXP1.append(CASE2_3_1_JOINED_MID_MEAN_MAX5)
+CASE_LIST_EXP1.append(CASE2_3_2_JOINED_MID_MEAN_MAX5_ELEV)
+###CASE_LIST_EXP1.append(CASE2_3_3_JOINED_MID_MEAN_MAX5_PROD)
+#CASE_LIST_EXP1.append(CASE2_4_1_JOINED_MID_MEAN_MIN5)
+#CASE_LIST_EXP1.append(CASE2_4_2_JOINED_MID_MEAN_MIN5_ELEV)
+###CASE_LIST_EXP1.append(CASE2_4_3_JOINED_MID_MEAN_MIN5_PROD)
+CASE_LIST_EXP1.append(CASE3_1_JOINED_ELEV)
+CASE_LIST_EXP1.append(CASE3_2_JOINED_PROD)
+
+# List of cases in experiment 2
+CASE_LIST_EXP2 = []
+#CASE_LIST_EXP2.append(CASE1_SINGLE)
+CASE_LIST_EXP2.append(CASE2_1_STAT)
+CASE_LIST_EXP2.append(CASE2_2_1_JOINED_MID_MEAN)
+CASE_LIST_EXP2.append(CASE2_2_2_JOINED_MID_MEAN_ELEV)
+###CASE_LIST_EXP2.append(CASE2_2_3_JOINED_MID_MEAN_PROD)
+#CASE_LIST_EXP2.append(CASE2_3_1_JOINED_MID_MEAN_MAX5)
+#CASE_LIST_EXP2.append(CASE2_3_2_JOINED_MID_MEAN_MAX5_ELEV)
+###CASE_LIST_EXP2.append(CASE2_3_3_JOINED_MID_MEAN_MAX5_PROD)
+CASE_LIST_EXP2.append(CASE2_4_1_JOINED_MID_MEAN_MIN5)
+CASE_LIST_EXP2.append(CASE2_4_2_JOINED_MID_MEAN_MIN5_ELEV)
+###CASE_LIST_EXP2.append(CASE2_4_3_JOINED_MID_MEAN_MIN5_PROD)
+#CASE_LIST_EXP2.append(CASE3_1_JOINED_ELEV)
+#CASE_LIST_EXP2.append(CASE3_2_JOINED_PROD)
+
 
 #  All the file names should start with this prefix string
 #FEATURE_FILE_PREFIX = "svm_traindata.txt.1a";
--- src/test_network_radar.py
+++ src/test_network_radar.py
@@ -1,3 +1,7 @@
+# NOTE1: Modify result_log_dir, training_data_dir to switch between feature directories.
+# NOTE2: Modify case_list to set the list of cases for doing the experiments.
+# NOTE3: To save the standard output to a file(eg. test_network_radar.stdout.log),
+#        Run -> Run Configurations... -> Common tab -> Standard input and output -> File; also check the "append" checkbox.
 import light_format_feature_loader
 import network
 import glob
@@ -10,25 +14,12 @@ rainfall_max_value = 100
 n_rainfall_indexes = 100 # for use in vectorized form of rainfall label (number of neurons for output layer)
 size_hidden_layer = 30 # Number of neurons for hidden layer
 
-case_list = []
-#case_list.append(CASE1_SINGLE)
-case_list.append(CASE2_1_STAT)
-case_list.append(CASE2_2_1_JOINED_MID_MEAN)
-case_list.append(CASE2_2_2_JOINED_MID_MEAN_ELEV)
-###case_list.append(CASE2_2_3_JOINED_MID_MEAN_PROD)
-#case_list.append(CASE2_3_1_JOINED_MID_MEAN_MAX5)
-#case_list.append(CASE2_3_2_JOINED_MID_MEAN_MAX5_ELEV)
-###case_list.append(CASE2_3_3_JOINED_MID_MEAN_MAX5_PROD)
-case_list.append(CASE2_4_1_JOINED_MID_MEAN_MIN5)
-case_list.append(CASE2_4_2_JOINED_MID_MEAN_MIN5_ELEV)
-###case_list.append(CASE2_4_3_JOINED_MID_MEAN_MIN5_PROD)
-#case_list.append(CASE3_1_JOINED_ELEV)
-#case_list.append(CASE3_2_JOINED_PROD)
+case_list = CASE_LIST_EXP2 # Refer to case_config.py
 
-#result_log_dir = "../log/" + SVMDATA_10minPrecAsLabel
+result_log_dir = "../log/" + SVMDATA_10minPrecAsLabel
 #result_log_dir = "../log/" + SVMDATA_10minPrecAsLabel_HourlyAggr
 #result_log_dir = "../log/" + SVMDATA_10minPrecAsLabel_FuzzyLabel
-result_log_dir = "../log/" + SVMDATA_10minPrecAsLabel_HourlyAggr_FuzzyLabel
+#result_log_dir = "../log/" + SVMDATA_10minPrecAsLabel_HourlyAggr_FuzzyLabel
 
 if not os.path.exists(result_log_dir):
     os.makedirs(result_log_dir)
@@ -40,10 +31,10 @@ previous_case_dir = case_list[0]
 result_log_file = result_log_dir + "/" + case_list[0] + ".log"
 log_fp = open(result_log_file, 'a')
 for case_dir in case_list:
-    #training_data_dir = "D:\\\\" + SVMDATA_10minPrecAsLabel + "\\" + case_dir
+    training_data_dir = "D:\\\\" + SVMDATA_10minPrecAsLabel + "\\" + case_dir
     #training_data_dir = "D:\\\\" + SVMDATA_10minPrecAsLabel_HourlyAggr + "\\" + case_dir
     #training_data_dir = "D:\\\\" + SVMDATA_10minPrecAsLabel_FuzzyLabel + "\\" + case_dir
-    training_data_dir = "D:\\\\" + SVMDATA_10minPrecAsLabel_HourlyAggr_FuzzyLabel + "\\" + case_dir
+    #training_data_dir = "D:\\\\" + SVMDATA_10minPrecAsLabel_HourlyAggr_FuzzyLabel + "\\" + case_dir
     result_log_file = result_log_dir + "/" + case_dir + ".log"
     
     if previous_case_dir != case_dir:
