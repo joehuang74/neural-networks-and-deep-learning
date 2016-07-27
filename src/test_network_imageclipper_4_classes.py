5b28ea5b09cf520389e462904f972166a1a16c82
joehuang74 <joehuang74@users.noreply.github.com>
2016-07-27 15:57:25 +0800

    Add another two training cases: 1. imageclipper.pkl.gz    four classes: bike, car, motorcycle, pedestrian 2. vehicle_2_classes_small.pkl.gz    two classes: vehicle or not

--- src/test_network_imageclipper_4_classes.py
+++ src/test_network_imageclipper_4_classes.py
@@ -1,15 +1,15 @@
-import mnist_loader_with_printed_msg
+import imageclipper_4_classes_loader
 import network
 
 #training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-training_data, validation_data, test_data = mnist_loader_with_printed_msg.load_data_wrapper()
+training_data, validation_data, test_data = imageclipper_4_classes_loader.load_data_wrapper()
 
 """ 
 Build a three-layer neural network given the number of neurons for each layer
  Parameters:
  [number of neurons for input layer], [number of neurons for hidden layer], [number of neurons for output layer]
 """
-net = network.Network([784, 30, 10])
+net = network.Network([784, 30, 4])
 
 """
 Perform stochastic gradient descent algorithm given necessary hyper-parameters
@@ -17,6 +17,6 @@ Perform stochastic gradient descent algorithm given necessary hyper-parameters
  [training_data], [number of epoches], [mini-batch size], [training rate], [test_data]
  net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 """
-net.SGD(training_data, 100, 30, 0.1, test_data=test_data)
+net.SGD(training_data, 1000, 30, 0.1, test_data=test_data)
 
 
